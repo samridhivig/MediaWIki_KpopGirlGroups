50eccb4d1e85da4165ea386457155f864b1bca7d
Samridhi Vig <samridhivigd.a.v@gmail.com>
2020-10-16 16:52:45 +0200

    Update kpop-news.js

--- kpop-news.js
+++ kpop-news.js
@@ -34,10 +34,10 @@ request.onload = function(){
           const card = document.createElement('div');
         card.setAttribute('class','news_card');
         
-        const thumbnail_container = document.createElement('div');
+       /* const thumbnail_container = document.createElement('div');
         thumbnail_container.setAttribute('class','thumbnail_container');
         const thumbnail = document.createElement('img');
-        thumbnail.src = thumbnail_array[i];
+        thumbnail.src = thumbnail_array[i];*/
         
         const news_headline = document.createElement('div');
         news_headline.setAttribute('class','news_headline');
@@ -46,9 +46,9 @@ request.onload = function(){
          a.innerHTML = title_array[i];
         
         container.appendChild(card);
-        card.appendChild(thumbnail_container);
+        //card.appendChild(thumbnail_container);
         card.appendChild(news_headline);
-        thumbnail_container.appendChild(thumbnail);
+        //thumbnail_container.appendChild(thumbnail);
         news_headline.appendChild(a);
     }
     
